99aaeeeddf7635328440de334827d24711a6c33c
Nikhil Saireddy Munagala <nikhilsai.munagala@gmail.com>
2025-02-18 15:06:24 -0500

    AWS Secrets Manager credentials successfully received

--- api/mailer.js
+++ api/mailer.js
@@ -29,7 +29,7 @@ const getEmailSecret = async () => {
   try {
     const response = await secretsManagerClient.send(
       new GetSecretValueCommand({
-        SecretId: "Email_Password",
+        SecretId: "PASSWORD",
         VersionStage: "AWSCURRENT",
       })
     );
